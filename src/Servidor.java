ad5f9c020f1c2e36ae7cd52d1283a74c3005b282
Luis Guilherme Xavier <luisxavierguilherme@gmail.com>
2025-06-09 15:45:26 -0300

    estrutura basica do lado do servidor

---
+++ src/Servidor.java
@@ -0,0 +1,21 @@
+import entidades.modelos.ConsultaServico;
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+
+
+public class Servidor {
+    public static void main(String[] args){
+        try{
+            ConsultaServico servico = new ConsultaServico();
+
+            LocateRegistry.createRegistry(1099);
+            
+            Naming.rebind("rmi://localhost:1099/ConsultaService", servico);
+
+            System.out.println("Servidor de consulta iniciado e aguardando conexões...");
+        } catch(Exception e) {
+            System.err.println("Erro ao iniciar o servidor: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/entidades/modelos/ConsultaServico.java
@@ -0,0 +1,43 @@
+package entidades.modelos;
+
+import entidades.interfaces.Consulta;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Date;
+import java.util.Map;
+
+public class ConsultaServico extends UnicastRemoteObject implements Consulta {
+    private final ConsultaBase consulta;
+
+    public ConsultaServico() throws RemoteException {
+       super();
+
+       this.consulta = new ConsultaVeterinaria();
+    }
+
+    @Override
+    public void realizarConsulta(Date data) throws RemoteException {
+        consulta.realizarConsulta(data);
+        System.out.println("Nova consulta agendada para:" + data);
+    }
+
+    @Override
+    public void cancelarConsulta(int id) throws RemoteException {
+        try {
+            consulta.cancelarConsulta(id);
+            System.out.println("Consulta " + id + " cancelada.");
+        } catch (IllegalArgumentException e) {
+            System.err.println("erro ao cancelar consulta:" + e.getMessage());
+            throw new RemoteException("erro ao cancelar consulta:", e);
+        }
+        
+    }
+
+    @Override
+    public Map<String,Integer> getMedicamentos(String animal) throws RemoteException {
+        System.out.println("Buscando medicamento para o animal: "+ animal);
+        return consulta.getMedicamentos(animal);
+    }
+
+
+}
